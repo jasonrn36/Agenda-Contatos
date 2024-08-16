06b352e77929e884eb2d957e3d0bff6047580926
jasonrn36 <jason.rn36@gmail.com>
2024-08-16 15:28:55 -0400

    update files

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const formContatos = document.getElementById('form-contatos');
+const contato = [];
+const numContato = [];
+
         // Esta parte adiciona linha e mantem //
         let linhas = '';
 formContatos.addEventListener('submit', function(e) {
 
     e.preventDefault();
+        adicionaLinha();
+
+});
+
+        function adicionaLinha() {
+                
 
         const inputNomeContato = document.getElementById('nome');
         const inputNumeroContato = document.getElementById('telefone');
+
+
+        if (contato.includes(inputNomeContato.value)) {
+                alert(`Um contato com o nome: ${inputNomeContato.value}, já existe`);
+        }else{
+
+                contato.push(inputNomeContato.value);
+
 
         // Esta parte adiciona uma coluna com o nome do contato e outra coluna com o numero do contato ///
         let linha = '<tr>';
@@ -15,6 +32,7 @@ formContatos.addEventListener('submit', function(e) {
 
         linhas += linha;
 
+}
         //Esta parte adiciona mais um conteúdo acima dentro da tabela linha a pós linha//
         const corpoTabela = document.querySelector('tbody');
         corpoTabela.innerHTML = linhas;
@@ -22,4 +40,4 @@ formContatos.addEventListener('submit', function(e) {
         inputNomeContato.value = '';
         inputNumeroContato.value = '';
 
-});
+}
